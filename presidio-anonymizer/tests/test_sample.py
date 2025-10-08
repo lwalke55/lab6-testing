08b449c36d1af325f18a3842801a07caea0cb470
Laron Walker <233176084+lwalke55@users.noreply.github.com>
2025-10-08 20:10:48 +0000

    uses pytest

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,14 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
-def test_sample_run_anonymizer():
+@pytest.mark.parametrize(
+        #fmt: off
+        'text,start,end',
+        [
+            ("My name is Bond",11,15),
+        ],
+)
+
+def test_sample_run_anonymizer(text,start,end):
     # replace the following line with your test
-    assert sample_run_anonymizer("My name is Bond.", 11, 15) == "text: My name is BIP.\nitems:\n[\t{'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}\n]"
+    assert sample_run_anonymizer(text,start,end) == "text: My name is BIP.\nitems:\n[\t{'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}\n]"
