5766095c9404f69872d4e4b6e85e9d0a8c0658be
Laron Walker <233176084+lwalke55@users.noreply.github.com>
2025-10-08 19:57:10 +0000

    wrote one test for test_sample.py file

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -3,4 +3,4 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
     # replace the following line with your test
-    pass
+    assert sample_run_anonymizer("My name is Bond.", 11, 15) == "text: My name is BIP.\nitems:\n[\t{'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}\n]"
