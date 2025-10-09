4269c8e5718cd61f7d228b5e4ccf3f93cf236cb3
Laron Walker <233176084+lwalke55@users.noreply.github.com>
2025-10-09 01:04:27 +0000

    testing

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,6 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
-
+"""
 @pytest.mark.parametrize(
         #fmt: off
         'text,start,end',
@@ -8,11 +8,11 @@ from presidio_anonymizer.sample import sample_run_anonymizer
             ("My name is Bond.",11,15,),
         ],
 )
-
-def test_sample_run_anonymizer(text,start,end):
+"""
+def test_sample_run_anonymizer():
     # replace the following line with your test
-    result = sample_run_anonymizer(text,start,end)
+    result = sample_run_anonymizer("My name is Bond.",11,15)
     assert result.text == "My name is BIP."
+    assert len(result.text) == len ("My name is BIP.")
     assert result.items[0].start == 11
     assert result.items[0].end == 14
-    assert len(result.text) == len("My name is BIP.")
